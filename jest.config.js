d3ac3ce128fcbd14c3eae21e69048b1b98505494
Gustavo Muñoz <timbergus@gmail.com>
2019-02-14 11:19:30 +0100

    Add coverage report.

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,18 @@
 module.exports = {
     notify: true,
+
+    // Coverage configuration to show the missing global summary
+    // in Jest output.
+
+    collectCoverage: true,
+    coverageReporters: ['lcov'],
+    collectCoverageFrom: ['src/*.js'],
+    coverageThreshold: {
+        global: {
+            branches: 0,
+            functions: 0,
+            lines: 0,
+            statements: 0,
+        },
+    },
 };
